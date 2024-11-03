c07724c92660f7484cd6107fbb2253010379e222
Separatehhh23 <separatehhh23@gmail.com>
2024-11-03 02:14:08 +0100

    .

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -2,7 +2,7 @@
 
 WALLPAPER_PATH="$(hyprctl hyprpaper listactive | awk 'NR==1 {print $3}')"
 
-cp WALLPAPER_PATH ~/.current_wallpaper
+cp "$WALLPAPER_PATH" ~/.current_wallpaper
 
 echo "" > ~/.config/hypr/hyprpaper.conf
 echo "preload = $WALLPAPER_PATH" >> ~/.config/hypr/hyprpaper.conf
