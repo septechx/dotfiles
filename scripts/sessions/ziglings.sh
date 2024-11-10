8cad39ff09c1ae27d8a50a4364cd43e713b48fb2
Separatehhh23 <separatehhh23@gmail.com>
2024-11-10 22:51:35 +0100

    Use bash instead of zsh

--- scripts/sessions/ziglings.sh
+++ scripts/sessions/ziglings.sh
@@ -1,13 +1,13 @@
-#! /bin/zsh
+#! /bin/bash
 
 SESSIONNAME="ziglings"
 DIR="$HOME/projects/learn/zig/exercises/exercises/"
 tmux has-session -t $SESSIONNAME &>/dev/null
 
-if [ $? != 0 ];              then
-  tmux new-session -s $SESSIONNAME -c $DIR -d
+if [ $? != 0 ]; then
+  tmux new-session -s $SESSIONNAME -c "$DIR" -d
   tmux send-keys -t $SESSIONNAME 'vim' C-m
-  tmux new-window -t $SESSIONNAME -c $DIR
+  tmux new-window -t $SESSIONNAME -c "$DIR"
 fi
 
 tmux attach -t $SESSIONNAME
